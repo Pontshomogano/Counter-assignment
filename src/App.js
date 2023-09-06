f97c2894c060a500d16f462a47597380b8319782
Pontsho Mogano <pontshomogano4@gmail.com>
2023-09-06 15:43:43 +0200

    counter-assignment-altschool

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ChevronUp } from "react-feather";
+import { ChevronsUp } from "react-feather";
+import { RotateCcw } from "react-feather";
+import { Hash } from "react-feather";
+import { ChevronsDown } from "react-feather";
+import { ChevronDown } from "react-feather";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const HandleIncrement = (event) => {
+    setCounter(counter + 1);
+  };
+
+  const HandleIncrementTen = (event) => {
+    setCounter(counter + 10);
+  };
+  const ResetHandle = (event) => {
+    setCounter(counter * 0);
+  };
+  const RandomHandle = (event) => {
+    setCounter(counter * 0);
+  };
+
+  const randomNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const randomClick = () => {
+    setCounter(randomNumberInRange(1, 100));
+  };
+
+  const decrementTen = (event) => {
+    setCounter(counter - 10);
+  };
+  const handleDecreaseOne = (event) => {
+    setCounter(counter - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <div className="current-v">Current value:</div>
+      <div className="label-counter">{counter}</div>
+      <button className="one-btn" onClick={HandleIncrement}>
+        {" "}
+        <ChevronUp />
+      </button>
+      <button className="ten-btn" onClick={HandleIncrementTen}>
+        <ChevronsUp />
+      </button>
+      <button className="zero-btn" onClick={ResetHandle}>
+        {" "}
+        <RotateCcw />
+      </button>
+      <button className="random-btn" onClick={randomClick}>
+        {" "}
+        <Hash />
+      </button>
+      <button className="tendown-btn" onClick={decrementTen}>
+        {" "}
+        <ChevronsDown />
+      </button>
+      <button className="onedown-btn" onClick={handleDecreaseOne}>
+        <ChevronDown />
+      </button>
+    </section>
   );
 }
-
 export default App;
